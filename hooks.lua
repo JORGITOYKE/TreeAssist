5611d8051d14983a6d683e68babf61cb0d6170cb
JORGITO_YKE <jorgito.yke.mitracraft@gmail.com>
2015-12-18 21:09:59 +0100

    fixed player dying whe breaks wood fast. make plugin to look like a survival behaviour. get only one sappling from the wood in contact with dirt block.

--- hooks.lua
+++ hooks.lua
@@ -6,18 +6,12 @@
 
 
 
-
-function OnPlayerLeftClick(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_BlockFace, a_Action)
+function OnPlayerBreakingBlock(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_BlockFace, a_BlockType, a_BlockMeta)
 	-- Check if the user should only be allowed to use survival with this plugin
 	if (g_Config.SurvivalOnly and not a_Player:IsGameModeSurvival()) then
 		return false
 	end
-	
-	-- Check if this is actualy a left click.
-	if (a_Action ~= 0) then
-		return false
-	end
-	
+		
 	-- Check if the blockface is correct
 	if (a_BlockFace == BLOCK_FACE_NONE) then
 		return false
@@ -31,23 +25,23 @@ function OnPlayerLeftClick(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_BlockFace, 
 	
 	-- Check if the block that is about to be digged up is wood
 	local World = a_Player:GetWorld()
-	local BlockType = World:GetBlock(a_BlockX, a_BlockY, a_BlockZ)
-	if (not ItemCategory.IsWood(BlockType)) then
+	local a_BlockType = World:GetBlock(a_BlockX, a_BlockY, a_BlockZ)
+	if (not ItemCategory.IsWood(a_BlockType)) then
 		return false
 	end
 	
-	local BlockMeta = World:GetBlockMeta(a_BlockX, a_BlockY, a_BlockZ)
+	local a_BlockMeta = World:GetBlockMeta(a_BlockX, a_BlockY, a_BlockZ)
 	
 	-- Initialize the collection class
-	local Collection = g_CollectionClass(a_Player, BlockType, BlockMeta)
+	local Collection = g_CollectionClass(a_Player, a_BlockType, a_BlockMeta)
 	
 	-- Go through each block above the current block until the block ID is different.
 	for PosY = a_BlockY, World:GetHeight(a_BlockX, a_BlockZ), 1 do
-		if (World:GetBlock(a_BlockX, PosY, a_BlockZ) ~= BlockType) then
+		if (World:GetBlock(a_BlockX, PosY, a_BlockZ) ~= a_BlockType) then
 			break;
 		end
 		
-		if (World:GetBlockMeta(a_BlockX, PosY, a_BlockZ) ~= BlockMeta) then
+		if (World:GetBlockMeta(a_BlockX, PosY, a_BlockZ) ~= a_BlockMeta) then
 			break;
 		end
 		
@@ -57,8 +51,9 @@ function OnPlayerLeftClick(a_Player, a_BlockX, a_BlockY, a_BlockZ, a_BlockFace, 
 	
 	Collection:Finish()
 	
-	if (g_Config.ReplantSapling) then
-		World:QueueSetBlock(a_BlockX, a_BlockY, a_BlockZ, E_BLOCK_SAPLING, GetSaplingMeta(BlockType, BlockMeta), g_Config.SaplingPlantTime)
+	local a_BlockType2 = World:GetBlock(a_BlockX, a_BlockY-1, a_BlockZ)
+	if (g_Config.ReplantSapling and a_BlockType2 == E_BLOCK_DIRT) then
+		World:QueueSetBlock(a_BlockX, a_BlockY, a_BlockZ, E_BLOCK_SAPLING, GetSaplingMeta(a_BlockType, a_BlockMeta), g_Config.SaplingPlantTime)
 	end
 end
 
